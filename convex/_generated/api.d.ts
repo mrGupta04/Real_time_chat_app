ca86bab3360c87f42eeff85da22efb5da7a403c6
adityafloneo <adithyaintern@floneo.co>
2026-02-25 02:49:43 +0530

    final push

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,13 +1,3 @@
-/* eslint-disable */
-/**
- * Generated `api` utility.
- *
- * THIS CODE IS AUTOMATICALLY GENERATED.
- *
- * To regenerate, run `npx convex dev`.
- * @module
- */
-
 import type * as conversations from "../conversations.js";
 import type * as lib_auth from "../lib/auth.js";
 import type * as messages from "../messages.js";
@@ -29,28 +19,10 @@ declare const fullApi: ApiFromModules<{
   typing: typeof typing;
   users: typeof users;
 }>;
-
-/**
- * A utility for referencing Convex functions in your app's public API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = api.myModule.myFunction;
- * ```
- */
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
 >;
-
-/**
- * A utility for referencing Convex functions in your app's internal API.
- *
- * Usage:
- * ```js
- * const myFunctionReference = internal.myModule.myFunction;
- * ```
- */
 export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
